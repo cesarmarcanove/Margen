6eb3ac86b654bf15611f4ef732575e98711929ec
cesar marcano <cesarmarcanove@users.noreply.github.com>
2021-03-08 13:50:01 -0400

    Add files via upload

---
+++ Margen3/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Margen de Ganancia y Margen Porcentual")> 
+<Assembly: AssemblyDescription("Aplicativo que permitan el Margen de Ganancia y el Margen Porcentual, nos permita en que porcentaje que subió el precio de un bien y/o servicio. Los precios basados bajo el Plan Inteligente Plus 1.1 GB en la Operadora de Telefonía Móvil Privada DIGITEL.")> 
+<Assembly: AssemblyCompany("Creado por Ing. Cesar Marcano")> 
+<Assembly: AssemblyProduct("Margen")> 
+<Assembly: AssemblyCopyright("Copyright © 2019")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("dc765578-f963-44f0-a163-5c3483682d8b")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de versión de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.2.0.0")> 
+<Assembly: AssemblyFileVersion("1.2.0.0")> 
